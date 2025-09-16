c83b2e39e5cb67d70cd6efa22566f593219da46e
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2025-09-16 23:03:38 +0530

    migrations

---
+++ drizzle/0000_low_zarda.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `item` (
+	`id` text PRIMARY KEY NOT NULL,
+	`d` text
+);
+--> statement-breakpoint
+CREATE TABLE `text` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text,
+	`place` text,
+	`deity` text,
+	`sect` text,
+	`typeof` text
+);
+--> statement-breakpoint
+CREATE TABLE `vocab` (
+	`word` text NOT NULL,
+	`rank` integer DEFAULT 1,
+	`langid` integer DEFAULT 0,
+	`soundslike` text
+);
