6c102e423b155ab0dd22c6e8d406bc76f8a4d600
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2025-09-18 21:15:29 +0530

    fix: increase maximum radius for geolocation search from 10 km to 50 km

--- src/search.ts
+++ src/search.ts
@@ -19,7 +19,7 @@ searchRoute.get('/',
       // User Location or Map Center
       latitude: z.coerce.number().min(-90).max(90).optional(),
       longitude: z.coerce.number().min(-180).max(180).optional(),
-      radius: z.coerce.number().min(0).max(10000).default(1000).optional(), // in meters (max 10 km)
+      radius: z.coerce.number().min(0).max(50000).default(1000).optional(), // in meters (max 50 km)
       // Pagination
       limit: z.number().min(1).max(100).default(20).optional(), // Result Limit
       offset: z.number().min(0).max(1000).default(0).optional(), // Result Offset
